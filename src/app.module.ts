325f06a75899dc5cc83f2ccaa07c07134e3b0ea7
RainyRooftopCoder <rainyrooftopcoder@gmail.com>
2025-05-15 10:28:16 +0900

    craate config ENV

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(
+        process.cwd(),
+        'src',
+        'env',
+        `.env.${process.env.NODE_ENV}`,
+      ),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  // console.log(configService);
+  // console.log(configService.get<number>('PORT'));
+
+  const port = configService.get<number>('PORT') ?? 3000;
+  await app.listen(port);
+  console.log(`📚 GuestBook Project 🚀Start Server PORT :: [${port}]`);
 }
 bootstrap();
